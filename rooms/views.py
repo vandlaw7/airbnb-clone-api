fa61cc199e58a88103507a1bc44bd2fbc24dacbf
vandlaw7 <vandlaw@naver.com>
2020-07-06 16:30:36 +0900

    #1.4 mid commit: using APIview

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 
-@api_view(["GET"])
-def list_rooms(request):
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
+class ListRoomsView(APIView):
 
-    return Response(data=serialized_rooms.data)
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
