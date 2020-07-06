5b55440fd698aac8e473650623631236dba37646
vandlaw7 <vandlaw@naver.com>
2020-07-06 16:23:17 +0900

    #1.3

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
+from users.serializers import TinySerializer
+from .models import Room
 
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+
+class RoomSerializer(serializers.ModelSerializer):
+
+    user=TinySerializer()
+
+    class Meta:
+        model = Room
+        fields = ("name", "price", "instant_book", "user")
