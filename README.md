e8cc81b36a24623f4b1954d8154ce5f389f2537d
vandlaw7 <vandlaw@naver.com>
2020-07-10 19:13:11 +0900

    #2.6 User part start! Meview and user_detail

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ REST & GraphQL API of the Airbnb Clone using Django REST Framework and Graphene 
 - [x] List Rooms
 - [x] See Room
 - [x] Create Room
-- [ ] Edit Room
-- [ ] Delete Room
-- [ ] Filter Rooms
+- [x] Edit Room
+- [x] Delete Room
+- [ ] Search Rooms
 - [ ] Search By Coords
 - [ ] Login
 - [ ] Create Account
 - [ ] Add Room to Favourites
-- [ ] See Favs
 - [ ] See Profile
 - [ ] Edit Profile
--- users/serializers.py
+++ users/serializers.py
@@ -2,9 +2,29 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ("groups", "user_permissions", "last_login",
-                   "password", "is_staff", "is_active", "is_superuser",
-                   "favs")
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost"
+        )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "groups",
+            "user_permissions",
+            "last_login",
+            "password",
+            "is_staff",
+            "is_active",
+            "is_superuser",
+            "date_joined",
+        )
