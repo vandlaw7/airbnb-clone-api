551485ce5f0ab0c9bbaca8ab2279fdae655ed70d
vandlaw7 <vandlaw@naver.com>
2020-07-21 21:42:18 +0900

    #2.9

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
 from rest_framework import status
+from rooms.models import Room
 from .models import User
 from .serializers import ReadUserSerializer, WriteUserSerialize
 from rooms.serializers import RoomSerializer
@@ -17,7 +18,8 @@ class MeView(APIView):
             return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerialize(request.user, data=request.data, partial=True)
+        serializer = WriteUserSerialize(
+            request.user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response()
@@ -39,7 +41,7 @@ def user_detail(request, pk):
 # @permission_classes([IsAuthenticated])
 # def toggle_fav(request):
 #     room = request.data.get("room")
-#     print(room) 
+#     print(room)
 
 class FavsView(APIView):
 
@@ -49,8 +51,23 @@ class FavsView(APIView):
         user = request.user
         serializer = RoomSerializer(user.favs.all(), many=True).data
         return Response(serializer)
-    
+
     # user의 favs를 업데이트하는 것일 뿐이다.새로 만드는 게 아니다.
     def put(self, request):
-        pass
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                user.save()
+                return Response()
 
+            except Room.DoesNotExist:
+                print(2)
+        else:
+            print(1)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
